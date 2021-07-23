89e03864c9195d2cef4009f1d22fc29eb58401bc
Liam Duxbury <liam.duxbury97@gmail.com>
2021-07-23 10:50:38 +0100

    Added custome user profile migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.model import AbstractBaseUser
-from django.contrib.auth.model import PermissionsMixin
-from django.contrib.auth.model import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
