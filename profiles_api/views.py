05474640c9a75d7d75f9ad65fe7c836734294ab4
GI Liam <liam@global-initiative.com>
2021-07-28 11:07:34 +0100

    Added user profiles and permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,10 @@ from django.core.exceptions import NON_FIELD_ERRORS
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers
-
+from profiles_api import permissions, serializers, models
 
 class HelloApiView(APIView) :
     """Test API view"""
@@ -93,3 +92,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset  = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
